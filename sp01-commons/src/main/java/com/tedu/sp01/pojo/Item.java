b1bf881bccfbac6b08870a5da2cc8153b0c70413
runoob <test@runoob.com>
2019-06-27 18:32:03 +0800

    1.commons

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,16 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	//±àºÅ
+	private Integer id;
+	//Ãû³Æ
+	private String name;
+	private Integer number;
+}
